985edb3be32aeea664ca870b4049b186fea17917
Ytallo de Lima Gadelha <ytallolg@gmail.com>
2016-11-24 23:46:04 -0300

    Criação de rotas sem/com parâmetros na consulta

--- hello-express/index.js
+++ hello-express/index.js
@@ -1,2 +1,18 @@
 // index.js
 const express = require("express");
+
+const app = express();
+
+app.get("/hello", (req,res) => res.send("Olá mundo :D"));
+
+app.get("/greet", (req,res) => {
+
+	//verificando se o parâmetro foi passado na consulta
+	if(req.query.nome)
+		res.send(`Olá, ${req.query.nome}!`);
+
+	else
+		res.send("Faltou o parâmetro");
+});
+
+app.listen(3000);
