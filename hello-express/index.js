398bc343884295ebe98b04f95aa7dcc02e4bfd9c
Ytallo de Lima Gadelha <ytallolg@gmail.com>
2016-11-25 00:26:57 -0300

    Trabalhando com parâmetros opcionais

--- hello-express/index.js
+++ hello-express/index.js
@@ -15,17 +15,31 @@ app.get("/greet", (req,res) => {
 		res.send("Faltou o parâmetro");
 });
 
+//verificando os parâmetros da consulta
 app.get("/queryparams", (req,res) => {
 
 	console.log(req.query);
 	res.send("Veja no console(terminal)");
 });
 
+//verificando os parâmetros de requisição
 app.get("/pathparams/:nome/:time", (req,res) => {
 
 	res.send(`Meu nome é ${req.params.nome} e meu time é o ${req.params.time}!`);
 	console.log(req.params);
 	console.log(`Meu nome é ${req.params.nome} e meu time é o ${req.params.time}!`);
+});
+
+//trabalhando com parâmetros opcionais
+app.get("/optional(/:maybe)?", (req,res) => {
+
+	if (req.params.maybe)
+		res.send("Call me " + req.params.maybe);
+
+	else
+		res.send("I just met you");
+
+	console.log(req.params);
 });
 
 app.listen(3000);
